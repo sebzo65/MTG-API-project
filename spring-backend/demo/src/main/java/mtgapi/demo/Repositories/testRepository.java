ff500ad5b0004c02594fd11ec7e68ca8a1540a81
Sebastian <sebzo65@gmail.com>
2022-02-09 10:07:36 +1100

    Created all of the Spring layers for the test and got them to work successfully. Next step is to test the endpoints with Postman, as well as add some of the layers for the actual MTG cards themselves

---
+++ spring-backend/demo/src/main/java/mtgapi/demo/Repositories/testRepository.java
@@ -0,0 +1,16 @@
+package mtgapi.demo.Repositories;
+
+import mtgapi.demo.Entities.testEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface testRepository extends JpaRepository<testEntity, Long> {
+
+    Optional<testEntity> findById(long id);
+    List<testEntity> findByName(String name);
+    List<testEntity> findByPower(Integer power);
+}
